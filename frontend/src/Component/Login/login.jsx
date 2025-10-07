fe1d3e63acac05054ba465630f8283584fc5e23f
ankurkdb17 <ankurkundu768@gmail.com>
2025-10-07 16:20:22 +0530

    Update login.jsx

--- frontend/src/Component/Login/login.jsx
+++ frontend/src/Component/Login/login.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './login.css';
+import {Link} from 'react-router-dom'
 
-const Login = () => {
+
+const Login = ({setloginmodel}) => {
+  const [loginField,setLoginField]=useState({"userName":"","password":""});
+  const handleOnchangeinput=(event,name)=>{
+    setLoginField(
+      {
+        ...loginField,[name]:event.target.value
+      }
+    )
+
+  }
+  
   return (
     <div className='login'>
         <div className="login_card">
@@ -11,7 +23,22 @@ const Login = () => {
             </div>
             <div className="login_credential">
               <div className="username_login">
-                
+                <input className="username_loginusername" value={loginField.userName}  onChange={(e)=>handleOnchangeinput(e,"userName")} placeholder="username" type="text" />
+              </div>
+               <div className="username_login">
+                <input className="username_loginusername" value={loginField.password} onChange={(e)=>handleOnchangeinput(e,"password")} placeholder="Password" type="text" />
+              </div>
+              
+            </div>
+            <div className="login_buttons">
+              <div className="loginbtn">
+                   Login
+              </div>
+               <Link to={'/signup/9974'} onClick={()=>setloginmodel()} className="loginbtn">
+                  SignUp
+              </Link>
+               <div className="loginbtn" onClick={()=>setloginmodel()}>
+                  Cancel
               </div>
             </div>
         </div>
