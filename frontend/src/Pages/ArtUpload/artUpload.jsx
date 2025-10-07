bb7ad6b922be35b834e7a57d389c373d360fd13f
ankurkdb17 <ankurkundu768@gmail.com>
2025-10-07 16:25:38 +0530

    Update artUpload.jsx

--- frontend/src/Pages/ArtUpload/artUpload.jsx
+++ frontend/src/Pages/ArtUpload/artUpload.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './artUpload.css'
 import {Link} from 'react-router-dom'
 
 const ArtUpload = () => {
+    const [inputField, setInputField] = useState({ "title": "", "description": "", "videoType": "", "thumbnail": "", "videoLink": "" })
+    const handleOnchangeinput=(event,name)=>{
+         setInputField(
+            {
+                ...inputField,[name]:event.target.value
+            }
+         )
+    }
   return (
     <div className='artUpload'>
         <div className="uploadbox">
@@ -11,9 +19,9 @@ const ArtUpload = () => {
                 Upload Arts
             </div>
             <div className="uploadForm">
-                <input type="text" placeholder="Title of Video" className='uploadFormInputs' />
-                <input type="text" placeholder="Description" className='uploadFormInputs' />
-                <input type="text" placeholder="Category" className='uploadFormInputs' />
+                <input type="text" value={inputField.title} onChange={(e)=>handleOnchangeinput(e,"title")} placeholder="Title of Video" className='uploadFormInputs' />
+                <input type="text"value={inputField.description} onChange={(e)=>handleOnchangeinput(e,"description")}placeholder="Description" className='uploadFormInputs' />
+                <input type="text"value={inputField.videoType}onChange={(e)=>handleOnchangeinput(e,"videoType")} placeholder="Category" className='uploadFormInputs' />
                 <div>Thumbnail<input type='file' accept="image/*"/></div>
                 <div>Video <input type='file' accept="video/mp4,video/webm,video/*"/></div>
             </div>
