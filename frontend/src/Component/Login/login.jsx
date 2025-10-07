826cbf9ee35ddc67c87a842d8dadf84f674bcb3b
ankurkdb17 <ankurkundu768@gmail.com>
2025-10-07 11:22:20 +0530

    Create login.jsx

---
+++ frontend/src/Component/Login/login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './login.css';
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <div className="login_card">
+            <div className="title_card">
+              <div className="navbar_title">ArtEcho</div>
+              Login
+            </div>
+            <div className="login_credential">
+              <div className="username_login">
+                
+              </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
